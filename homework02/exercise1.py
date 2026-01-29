9dd337e67434311adfe033cb49a520c6f3e87e52
prathamppatel <prathamppatel@utexas.edu>
2026-01-29 06:27:57 +0000

    committing exercise1.py

--- homework02/exercise1.py
+++ homework02/exercise1.py
@@ -1,4 +1,11 @@
-#int_list = [1, 2, 3, 4, 5, 6 , 7, 8 , 9, 10]
 
-#def even_or_odd(int_list):
- #   for num in int_list: 
+def even_or_odd(int_list):
+    for num in int_list: 
+        if num%2 >= 1:
+            print(f'{num} is odd!')
+        elif num%2 == 0:
+            print(f'{num} is even!')
+
+int_list = [1, 2, 3, 4, 5, 6 , 7, 8 , 9, 10]
+
+even_or_odd(int_list)
