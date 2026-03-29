455fb565e0ecf962f458324c1ecdd08320439ac8
prathamppatel <prathamppatel@utexas.edu>
2026-03-29 00:14:17 +0000

    trial 2 for CI

--- homework08/app.py
+++ homework08/app.py
@@ -6,6 +6,7 @@ server = app.server
 app.layout = [html.Div(children='Hello, World!')]
 
 # just a comment so theres a change
+# trying again
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8050, debug=True)
